39910062527b9f4e17d64a5609ed64b26a5af4ba
José Medeiros <joseprsm@gmail.com>
2022-09-26 17:54:06 +0100

    add upload endpoint

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
-from fastapi import FastAPI, Request
+import io
+import os
+
+import requests
+from fastapi import FastAPI, Request, UploadFile
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from starlette.responses import StreamingResponse
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -10,3 +15,10 @@ templates = Jinja2Templates(directory="templates")
 async def home(request: Request):
     data = {}
     return templates.TemplateResponse("page.html", {"request": request, "data": data})
+
+
+@app.post("/upload")
+async def upload(file: UploadFile):
+    img_bytes: bytes = file.file.read()
+    resp = requests.post(os.environ["FOREGROUND_URL"], files={"file": img_bytes})
+    return StreamingResponse(io.BytesIO(resp.content), media_type="image/png")
